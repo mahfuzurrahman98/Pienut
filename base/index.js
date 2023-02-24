04a0c40cfa470fbad725e05ba288945b255fa172
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-02-24 22:05:39 +0600

    v1.1.1

---
+++ base/index.js
@@ -0,0 +1,7 @@
+export const Controller = require('./Controller.js');
+export const Model = require('./Model.js');
+export const Route = require('./Route.js');
+export const Validation = require('./Validator.js');
+export const Pienut = require('./Pienut.js');
+export const Database = require('./Database.js');
+export const Password = require('./utils/Password.js');
--- index.js
+++
@@ -1,7 +0,0 @@
-export const Controller = require('./base/Controller.js');
-export const Model = require('./base/Model.js');
-export const Route = require('./base/Route.js');
-export const Validation = require('./base/Validator.js');
-export const Pienut = require('./base/Pienut.js');
-export const Database = require('./base/Database.js');
-export const Password = require('./base/utils/Password.js');
