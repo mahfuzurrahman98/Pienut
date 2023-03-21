1a30cf192ceb5e6ec7daffe23becb25998f9f1c7
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-03-21 22:17:47 +0600

    index.js brought out of base dir

---
+++ index.js
@@ -0,0 +1,10 @@
+import Auth from './base/auth/Auth.js';
+import Controller from './base/Controller.js';
+import Model from './base/Model.js';
+import Pienut from './base/Pienut.js';
+import Route from './base/Route.js';
+import Password from './base/utils/Password.js';
+import Validator from './base/Validator.js';
+
+export { Controller, Model, Validator, Route, Password, Auth };
+export default Pienut;
